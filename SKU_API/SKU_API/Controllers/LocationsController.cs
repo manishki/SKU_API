c38004b9148b9e0157a9f6148a94c18e818396b9
Manish Kumar Choudhary <ManishKumar.Choudhary@thomsonreuters.com>
2019-12-27 17:21:44 +0530

    Required APIs

---
+++ SKU_API/SKU_API/Controllers/LocationsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SKU_API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class LocationsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var obj = new { Name = "Manish", Age = 28, Education= "MCA" };
+            
+            return Ok(obj);
+        }
+    }
+}
